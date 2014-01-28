68bffa63060c21ee517842f158544e06578cb326
Dan Sadaka <dan@web-site1.com>
2014-01-27 23:35:04 -0500

    almost done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,56 @@
 //= require bootstrap
 //= require_tree .
 
-$("a").on('click', function() {
-    alert("You clicked " + this.data("move"));
-} )
+window.player_score   = 0;
+window.opponent_score = 0;
+
+$(document).ready(function() {
+  $("#moves img").on('click', function() {
+//    alert("You clicked " + $(this).data("move"));
+    var opponent_moves  = ['rock','paper','scissors'];
+    var opponent_move   = opponent_moves[randomIntFromInterval(0, 2)];
+    var player_move     = $(this).data("move");
+
+    if (player_move != opponent_move) {
+      switch(player_move + opponent_move) {
+        case 'paperrock' : case 'scissorspaper' : case 'rockscissors' :
+          player_score += 1;
+          $winning_panel = $('#player-panel');
+          break;
+        default:
+          opponent_score += 1;
+          $winning_panel = $('#opponent-panel');
+          break;
+      }
+      displayScores($winning_panel);
+    } else {
+      handleTie()
+    }
+
+    $("#player-img").attr("src", "/assets/" + player_move + "L.jpg").show();
+    $("#opponent-img").attr("src", "/assets/" + opponent_move + "R.jpg").show();
+  } )
+})
+
+function randomIntFromInterval(min,max)
+{
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function displayScores($winning_panel) {
+  clearSuccess();
+  $winning_panel.addClass("panel-success");
+  $('#opponent-score').html(opponent_score).show();
+  $('#player-score').html(player_score).show();
+}
+
+function handleTie() {
+  clearSuccess();
+
+  $("#who-won").html("Tie").show()
+}
+
+function clearSuccess() {
+  $(".panel").removeClass("panel-success")
+  $("#who-won").html("&nbsp;").show();
+}
